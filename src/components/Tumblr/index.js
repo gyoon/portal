7f2b1ac64060f8bdb9de67abf25614fd0488c58a
changgyun.woo <cg25.woo@samsung.com>
2016-10-28 18:23:30 +0900

    masonry ui in progress
    Signed-off-by: changgyun.woo <cg25.woo@samsung.com>

--- src/components/Tumblr/index.js
+++ src/components/Tumblr/index.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import jsonp from 'jsonp';
+import Masonry from 'react-masonry-component';
 import tumblrAPI from './tumblrList.json';
 import './tumblr.scss';
+
+let masonryOptions = {
+    transitionDuration: 0
+};
 
 {
     /*const urlData = 'http://api.tumblr.com/v2/blog/conrad-woo.tumblr.com/posts?' +
@@ -35,14 +40,34 @@ export default class Tumblr extends Component {
         axios.get(tumblrAPI)
             .then(res => {
                 this.setState({item: res.data.response.posts})
-                console.log(this.state.item)
+
             });
     }
 
 	render(){
+        console.log(this.state.item)
+        var childElements = this.state.item.map(function(element, index){
+            return (
+                <li className="image-element-class" key={index}>
+                    <img width="100px" src={element.photos[0].original_size.url} />
+                </li>
+            );
+        });
+
 		return (
 			<div className="tumblrContainer">
 			    <h2>Tumblr</h2>
+
+                <Masonry
+                    className={'my-gallery-class'} // default ''
+                    elementType={'ul'} // default 'div'
+                    options={masonryOptions} // default {}
+                    disableImagesLoaded={false} // default false
+                    updateOnEachImageLoad={false} // default false and works only if disableImagesLoaded is false
+                >
+                    {childElements}
+                </Masonry>
+
             </div>
 		)
 	}
