c1fd8cfe702cf571250e3b8c9a25aa275ccb99c4
gyoon <woosys25@gmail.com>
2017-04-14 01:24:03 +0900

    data grid

--- src/view/Projects/index.js
+++ src/view/Projects/index.js
@@ -2,80 +2,128 @@ import React, { Component } from 'react';
 import { Link } from 'react-router';
 import './projects.scss';
 
+import ReactDataGrid from 'react-data-grid';
+const { Toolbar, Data: { Selectors } } = require('react-data-grid-addons');
+
 export default class Projects extends Component {
 
 	constructor() {
 		super();
+        this._columns = [
+            {
+                key: 'id',
+                name: 'ID',
+                filterable: true,
+                sortable: true
+            },
+            {
+                key: 'title',
+                name: 'Title',
+                filterable: true,
+                sortable: true
+            },
+            {
+                key: 'count',
+                name: 'Count',
+                filterable: true,
+                sortable: true
+            }];
+
+        this.state = {
+            rows: this.createRows(),
+            originalRows: this.createRows(),
+            filters: {}
+        };
 	}
 
+
+    createRows() {
+        let rows = [];
+        rows.push(
+            {
+                id: 1,
+                title: 'Title ' + 1,
+                count: 10
+            },
+            {
+                id: 3,
+                title: 'Title ' + 3,
+                count: 30
+            },
+            {
+                id: 2,
+                title: 'Title ' + 2,
+                count: 20
+            }
+        );
+
+        return rows;
+    }
+
+    getRows() {
+        return Selectors.getRows(this.state);
+    }
+
+    getSize() {
+        return this.getRows().length;
+    }
+
+    onRowClick(rowIdx, row) {
+        console.log(row)
+    }
+
 	render(){
+
+        const rowGetter = (rowIdx) => {
+            const rows = this.getRows();
+            return rows[rowIdx];
+            //return this.state.rows[i];
+        };
+
+        const handleFilterChange = (filter) => {
+            let newFilters = Object.assign({}, this.state.filters);
+            if (filter.filterTerm) {
+                newFilters[filter.column.key] = filter;
+            } else {
+                delete newFilters[filter.column.key];
+            }
+
+            this.setState({ filters: newFilters });
+        };
+
+        const onClearFilters = () => {
+            this.setState({ filters: {} });
+        };
+
+        const handleGridSort = (sortColumn, sortDirection) => {
+            const comparer = (a, b) => {
+                if (sortDirection === 'ASC') {
+                    return (a[sortColumn] > b[sortColumn]) ? 1 : -1;
+                } else if (sortDirection === 'DESC') {
+                    return (a[sortColumn] < b[sortColumn]) ? 1 : -1;
+                }
+            };
+
+            let originalRows = this.state.originalRows.slice(0);
+            const rows = sortDirection === 'NONE' ? originalRows : this.state.rows.sort(comparer);
+
+            this.setState({ rows });
+        };
+
 		return(
-			<div className="col-sm-7 blog-main">
-			<div className="contact-content">
-					<div className="row">
-						{
-							<div className="col-lg-12">
-									<h2 className="page-header">Project Tire(d)</h2>
-										<div className="col-lg-3 col-md-4 col-xs-6 thumb">
-												<Link className="thumbnail" to="/portal/projects/projectTired">
-													<img className="img-responsive heighted" src={require('../../assets/tired.jpg')} alt="no image available!" />
-												</Link>
-										</div>
-										<p className='text-size'>Development of an Engine with basic implementations to control a virtual 3D Tire. C++ and OpenGL were used in this project.</p>
-										<Link className='gray' to="/portal/portal/projectTired">
-											<p className='text-size-link'>read more...</p>
-										</Link>
-										<br/>
-										<br/>
-							</div>
-						}
-						{
-							<div className="col-lg-12">
-									<h2 className="page-header">BackOffice</h2>
-										<div className="col-lg-3 col-md-4 col-xs-6 thumb">
-												<Link className="thumbnail" to="/portal/projects/backOffice">
-													<img className="img-responsive heighted" src={require('../../assets/not_available.jpeg')}  alt="no image available!" />
-												</Link>
-										</div>
-										<p className='text-size'>Development of an MVC CMS application in C#.</p>
-										<Link className='gray' to="/portal/projects/backOffice">
-											<p className='text-size-link'>read more...</p>
-										</Link>
-										<br/>
-										<br/>
-										<br/>
-							</div>
-						}
-						{
-							<div className="col-lg-12">
-									<h2 className="page-header">SWEChan</h2>
-										<div className="col-lg-3 col-md-4 col-xs-6 thumb">
-												<Link className="thumbnail" to="/portal/projects/sweChan">
-													<img className="img-responsive heighted" src={require('../../assets/not_available.jpeg')}  alt="no image available!" />
-												</Link>
-										</div>
-										<p className='text-size'>Development of a simple content board (similar to 4chan.org) using the MVVM pattern in C#.</p>
-										<Link className='gray' to="/portal/projects/sweChan">
-											<p className='text-size-link'>read more...</p>
-										</Link>
-										<br/>
-										<br/>
-							</div>
-						}
-						{
-							// <div className="col-lg-12">
-							// 		<h2 className="page-header">Project Dinger</h2>
-							// 			<div className="col-lg-3 col-md-4 col-xs-6 thumb">
-							// 					<Link className="thumbnail" to="/portal/projects/projectDinger">
-							// 						<img className="img-responsive heighted" src={require('../../assets/not_available.jpeg')}  alt="no image available!" />
-							// 					</Link>
-							// 			</div>
-							// 			<p>This Android application is a multi-protocol instant messenger application on Android devices, which should combine different messenger protocols and use them to send broadcast messages to a group of friends in those messenger systems. (Still under development)</p>
-							// </div>
-						}
-					</div>
-			</div>
-			</div>
+            <ReactDataGrid
+                onGridSort={handleGridSort}
+                enableCellSelect={true}
+                columns={this._columns}
+                rowGetter={rowGetter}
+                rowsCount={this.getSize()}
+                //rowsCount={this.state.rows.length}
+                minHeight={500}
+                toolbar={<Toolbar enableFilter={true}/>}
+                onAddFilter={handleFilterChange}
+                onClearFilters={onClearFilters}
+                onRowClick={this.onRowClick}
+            />
 		);
 	}
 }
